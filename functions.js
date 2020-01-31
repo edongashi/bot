89ed72392e120c20e36826c6a7a55f22381721a8
Edon Gashi <edon@aircloak.com>
2020-02-01 00:10:03 +0100

    Initial commit

---
+++ functions.js
@@ -0,0 +1,81 @@
+const db = require('./db')
+const { isEqual, union, difference } = require('lodash')
+
+function toArray(val) {
+  return Array.isArray(val) ? val : [val]
+}
+
+function getRoles(userId) {
+  return db
+    .get('users')
+    .getById(userId)
+    .get('roles')
+    .value() || []
+}
+
+function hasRole(userId, roles) {
+  roles = toArray(roles)
+  return db
+    .user(userId)
+    .every(user =>
+      user.roles && roles.every(r => user.roles.includes(r))
+    )
+    .value()
+}
+
+function addRoles(userId, roles) {
+  roles = toArray(roles)
+  db
+    .user(userId)
+    .onEach(user => user
+      .defaults({ roles: [] })
+      .update('roles', r => union(r, roles))
+    )
+    .write()
+}
+
+function removeRoles(userId, roles) {
+  roles = toArray(roles)
+  db
+    .user(userId)
+    .onEach(user => user
+      .defaults({ roles: [] })
+      .update('roles', r => difference(r, roles))
+    )
+    .write()
+}
+
+function clearRoles(userId) {
+  db
+    .user(userId)
+    .onEach(user => user.set('roles', []))
+    .write()
+}
+
+function getData(userId, key, value) {
+  db
+    .user(userId)
+    .first()
+    .get(['data', key])
+    .value()
+}
+
+function setData(userId, key, value) {
+  db
+    .user(userId)
+    .onEach(user => user
+      .defaults({ data: {} })
+      .set(['data', key], value)
+    )
+    .write()
+}
+
+module.exports = {
+  getRoles,
+  hasRole,
+  addRoles,
+  removeRoles,
+  clearRoles,
+  getData,
+  setData
+}
---
+++ index.js
@@ -0,0 +1,99 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const { buildContext } = require('./context')
+const { getRoles, addRoles, removeRoles, clearRoles } = require('./functions')
+
+const privileged = fn => ctx => {
+  if (ctx.hasRole('admin')) {
+    fn(ctx)
+  } else {
+    return ctx.reply('no permission!')
+  }
+}
+
+const split = str => str.trim().split(/\s+/).map(x => x.trim()).filter(x => x)
+const delim = '```'
+const jsBlock = code => '```js\n' + code + '\n```'
+
+function handleGetUserRoles({ reply, mentions }) {
+  const roles = getRoles(mentions[0]).join(', ')
+  if (roles) {
+    reply(roles)
+  } else {
+    reply('<no roles>')
+  }
+}
+
+function handleGetRoles({ reply, sender }) {
+  const roles = getRoles(sender).join(', ')
+  if (roles) {
+    reply(roles)
+  } else {
+    reply('<no roles>')
+  }
+}
+
+function handleAddRoles({ mentions, reply, matches }) {
+  addRoles(mentions, split(matches[1]))
+  reply('done!')
+}
+
+function handleRemoveRoles({ mentions, reply, matches }) {
+  removeRoles(mentions, split(matches[1]))
+  reply('done!')
+}
+
+function handleRemoveAllRoles({ mentions, reply, matches }) {
+  clearRoles(mentions)
+  reply('done!')
+}
+
+function evalInContext(js, context) {
+  return function (str) {
+    return eval(str);
+  }.call(context, 'with(this) { ' + js + ' }');
+}
+
+function exec(ctx) {
+  const { message } = ctx
+  const index = message.indexOf('```js')
+  const end = message.lastIndexOf(delim)
+  const code = message.substring(index + '```js'.length, end)
+
+  try {
+    evalInContext(code, ctx)
+  } catch (e) {
+    console.error(e)
+    ctx.reply('error: ' + e.message)
+  }
+}
+
+const commands = [
+  [/^!roles? (?:<@!\S+?>)$/i, privileged(handleGetUserRoles)],
+  [/^!roles$/i, handleGetRoles],
+  [/^!addroles?\s+(?:<@!\S+?>)(?:\s+<@!\S+?>)*((?:\s+[a-z]+)*)$/i, privileged(handleAddRoles)],
+  [/^!rmroles?\s+(?:<@!\S+?>)(?:\s+<@!\S+?>)*((?:\s+[a-z]+)*)$/i, privileged(handleRemoveRoles)],
+  [/^!clroles?\s+(?:<@!\S+?>)(?:\s+<@!\S+?>)*$/i, privileged(handleRemoveAllRoles)],
+  [/^!exec(?:\s+<@!\S+?>)*\s+```js/i, privileged(exec)]
+]
+
+client.on('message', msg => {
+  if (msg.author.bot) {
+    return
+  }
+
+  if (msg.content && typeof msg.content === 'string') {
+    const content = msg.content.trim()
+
+    for (const [pattern, callback] of commands) {
+      const matches = content.match(pattern)
+      if (matches) {
+        const ctx = buildContext(msg, matches)
+        callback(ctx)
+        return
+      }
+    }
+  }
+})
+
+client.login(process.env.BOT_TOKEN)
